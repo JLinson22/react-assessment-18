8f067ce51d52461261b5539f85c97885436f904a
Jacob Linson <jlinson16@gmail.com>
2023-02-23 10:42:10 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+//import logo from './logo.svg';
 import './App.css';
-import Instructions from './components/Instructions'
+//import Instructions from './components/Instructions'
 import Comments from './components/Comments'
 
 function App() {
+
+  //const [comments, setComments] = useState([])
+    
+  // useEffect(() => {
+  //     const getData = async () => {
+  //         const response = await fetch('https://jsonplaceholder.typicode.com/comments')
+  //         const data = await response.json()
+  //         setComments(data)
+  //         console.log(data)
+  //     }
+  //     getData()
+  // }, [])
+
   return (
     <div className="container">
-      <Instructions />
+      {/* <Instructions /> */}
       <Comments />
     </div>
   );
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react'
+
 const Comments = () => {
     // Insturction:
     // Use endpoint: https://jsonplaceholder.typicode.com/comments to get a list of comments. 
@@ -6,10 +8,29 @@ const Comments = () => {
 
     // you will need to use the useEffect hook. Do this in async / await syntax.
 
+    const [comments, setComments] = useState([])
+    
+    useEffect(() => {
+        const getData = async () => {
+            const response = await fetch('https://jsonplaceholder.typicode.com/comments')
+            const data = await response.json()
+            setComments(data)
+        }
+        getData()
+    }, [])
+
+    const handleClick = (e) => {
+        console.log(e)
+    }
 
     return(
-        // Replace this with your code.
-        <h1>Comments component</h1>
+    <>
+    {comments.map(comment => (
+        <div key={comment.id} onClick={handleClick}>
+            {comment.body}
+        </div>
+    ))}
+    </>     
     )
 }
 
